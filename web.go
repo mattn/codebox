2402084f8fd3bdbb022e67a72762b80374053cc4
mattn.jp@gmail.com <mattn.jp@gmail.com>
2014-01-15 01:21:55 +0000

    web.go

--- web.go
+++ web.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+    "github.com/hoisie/web"
+    "os"
+)
+
 func main() {
-    println("foo")
+    web.Get("/", func() string {
+        return "hello world"
+    })
+    web.Run(":" + os.Getenv("PORT"))
 }
